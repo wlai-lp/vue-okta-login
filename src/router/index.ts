409874193f51f42edf78c2f90993a4bb795c0cc5
wlai-lp <wlai+gh@liveperson.com>
2024-10-09 16:40:51 -0400

    passing json

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Profile from '../components/Profile.vue'
-import JsonRender from '../components/JsonRender.vue'
 
 const routes = [
   { path: '/', component: Login },
   { path: '/profile', component: Profile },
   { path: '/login/callback', component: Profile },
-  { path: '/json', component: JsonRender },
 ]
 
 const router = createRouter({
