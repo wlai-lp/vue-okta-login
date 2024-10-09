aedb41370fcf6c4b3c0d1157f44f56b4d1cc2f16
wlai-lp <wlai+gh@liveperson.com>
2024-10-09 14:33:29 -0400

    move the button up

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,15 +5,16 @@
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Welcome, {{ userInfo.name }}!
         </h2>
-        <!-- JsonRenderer Component -->
-        <div class="mt-8">
-          <JsonRenderer :jsonString="userJson" />
-          <div>
+        <div>
             <button @click="logout"
               class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Logout
             </button>
           </div>
+        <!-- JsonRenderer Component -->
+        <div class="mt-8">
+          <JsonRenderer :jsonString="userJson" />
+          
         </div>
 
         <!-- <div class="mt-8 space-y-6">
